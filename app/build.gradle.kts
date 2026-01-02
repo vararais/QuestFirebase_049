610164c05a700bef760e6e059a022476cf4b5954
vararais <varadigmaisnann@gmail.com>
2026-01-02 20:08:15 +0700

    Revisi build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
 
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.androidx.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
