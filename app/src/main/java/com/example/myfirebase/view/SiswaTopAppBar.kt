9d7fe83d1a7c2d1d4f366ca8b406a696a6890e18
vararais <varadigmaisnann@gmail.com>
2026-01-02 22:27:02 +0700

    Menambah siswa top app bar

--- app/src/main/java/com/example/myfirebase/view/SiswaTopAppBar.kt
+++ app/src/main/java/com/example/myfirebase/view/SiswaTopAppBar.kt
@@ -1,5 +1,7 @@
 package com.example.myfirebase.view
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
