22686f8fa10af57ba7e78fa70604712d0261ba20
vararais <varadigmaisnann@gmail.com>
2026-01-02 20:09:22 +0700

    Menambah Destinasi Edit

---
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
@@ -0,0 +1,10 @@
+package com.example.myfirebase.view.route
+
+import com.example.myfirebase.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
