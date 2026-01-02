ef4a77c1a288ebfd2b67b7ae0c861a33942bec51
vararais <varadigmaisnann@gmail.com>
2026-01-02 20:09:14 +0700

    Menambah Destinasi Detail

---
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.myfirebase.view.route
+
+import com.example.myfirebase.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
