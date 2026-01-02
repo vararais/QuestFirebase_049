d40a77103bbedd30b0c936f0a5d99307d6ce97da
vararais <varadigmaisnann@gmail.com>
2026-01-02 22:26:50 +0700

    Memanggil file utama

--- app/src/main/java/com/example/myfirebase/MainActivity.kt
+++ app/src/main/java/com/example/myfirebase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myfirebase.ui.theme.MyFirebaseTheme
+import com.example.myfirebase.view.controlNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirebaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
