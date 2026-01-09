857886a7e511f7473f7c4650121f111385ff1e12
vararais <varadigmaisnann@gmail.com>
2026-01-09 18:24:34 +0700

    Menambahkan Penyedia View Model

--- app/src/main/java/com/example/myfirebase/view/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/view/viewmodel/PenyediaViewModel.kt
@@ -1,18 +1,44 @@
 package com.example.myfirebase.view.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myfirebase.repositori.AplikasiDataSiswa
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as AplikasiDataSiswa)
 
 object PenyediaViewModel {
+
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-        // Tambahkan initializer untuk Detail/EditViewModel jika sudah dibuat nanti
+
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
